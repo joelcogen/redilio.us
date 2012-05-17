24e3a076f00432c62a5d9ff3f74c3e45ed3a43a3
Joel Cogen <joel@joelcogen.com>
2012-05-17 21:56:35 +0200

    rdl

--- main.js
+++ main.js
@@ -4,5 +4,16 @@ $(function(){
     node.attr('href', 'mailto:'.concat(node.data('user'), '@', node.data('domain'), '.', node.data('tld')));
   });
 
+  setInterval(function(){
+    $('footer em').eq(0).text('d');
+    $('footer em').eq(1).text('l');
+    $('footer em').eq(2).text('r');
+    setTimeout(function(){
+      $('footer em').eq(0).text('r');
+      $('footer em').eq(1).text('d');
+      $('footer em').eq(2).text('l');
+    }, 500);
+  }, 10000);
+
   $(document).konami({callback: function(){sonic_rainboom()}});
 });
